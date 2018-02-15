4908ef6506bdcc27707b3961578512d9e1aa7515
Steve Hawkins <steve@slhawkins.com>
2018-02-14 19:11:56 -0600

    Added timer

--- solve.js
+++ solve.js
@@ -1,6 +1,20 @@
+function clock(start) {
+  if ( !start ) return process.hrtime();
+  var end = process.hrtime(start);
+  return Math.round((end[0]*1000) + (end[1]/1000000));
+}
+
+
 // solve.js
 var solver = require('./sudoku_solver')
 var board =
   '010008769000073028702600300007390000406000503000062400008001902570920000239800010'
 
-var solution = solver.solveSudoku(board)
+const runs = {}
+for (let i = 0; i < 3; i++) {
+  let t0 = clock()
+  solver.solveSudoku(board)
+  let duration = clock(t0)
+ runs[i] = "Call to solveSudoku() took " + duration + " milliseconds."
+}
+console.log(runs)
